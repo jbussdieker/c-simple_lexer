b40413d465a2f3200d22e4c8a5fe26212895b3f4
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-01 10:07:02 -0700

    More lexer stuff

--- lexer.c
+++ lexer.c
@@ -1,6 +1,60 @@
 #include <stdio.h>
 #include <string.h>
 
+#define M_WORD		1
+#define M_SUBSTRING	2
+#define M_BEGINNING	3
+#define M_CASE		4
+#define M_NOT		5
+
+int enum_matchers(const char *matcher, int *type, const char **param, int *s) {
+	const char *p = matcher;
+	const char *e;
+
+	if (*p != ';')
+		return 0;
+
+	p++;
+
+	if (strncmp(p, "w=\"", 3) == 0) {
+		p += 3;
+		for (e = p; *e != '\"' || e[-1] == '\\'; e++)
+			continue;
+		*param = p;
+		*s = (int)(e-p);
+		*type = M_WORD;
+		p = e + 1;
+	} else if (strncmp(p, "s=\"", 3) == 0) {
+		p += 3;
+		for (e = p; *e != '\"' || e[-1] == '\\'; e++)
+			continue;
+		*param = p;
+		*s = (int)(e-p);
+		*type = M_SUBSTRING;
+		p = e + 1;
+	} else if (strncmp(p, "b=\"", 3) == 0) {
+		p += 3;
+		for (e = p; *e != '\"' || e[-1] == '\\'; e++)
+			continue;
+		*param = p;
+		*s = (int)(e-p);
+		*type = M_BEGINNING;
+		p = e + 1;
+	} else if (*p == 'c') {
+		*s = 0;
+		*type = M_CASE;
+		p++;
+	} else if (*p == 'n') {
+		*s = 0;
+		*type = M_NOT;
+		p++;
+	} else {
+		// Invalid matcher
+		return -1;
+	}
+	return p - matcher;
+}
+
 int enum_fields(const char *fv, const char **m, int *s) {
     const char *f, *b;
     enum state {
@@ -92,10 +146,43 @@ int run_enum_fields_test(const char *example, const char **expect, int count) {
     }
 
     if (i < count) {
-	printf("\nERROR: Not enough matches. Expected %d more.\n\n", count - i);
+	printf("\nERROR: Not enough matches. Expected %d but got %d.\n\n", count, i);
 	return 1;
     }
 
+    return 0;
+}
+
+int run_enum_matchers_test(const char *example, int *expect_type, const char **expect_param, int count) {
+    const char *match;
+    int size;
+    int type;
+    int read = 0;
+    int offset = 0;
+    int i = 0;
+
+    while (read = enum_matchers(example + offset, &type, &match, &size)) {
+	if (i >= count) {
+	    printf("\nERROR: Too many matches\n\n");
+	    return 1;
+	} else if (expect_type[i] != type) {
+	    printf("\nERROR: Expected type (%d) == (%d)\n\n", type, expect_type[i]);
+	    return 1;
+	} else if (strlen(expect_param[i]) != size) {
+	    printf("\nERROR: Expected (%.*s) == (%s)\n\n", size, match, expect_param[i]);
+	    return 1;
+	} else if (memcmp(match, expect_param[i], size)) {
+	    printf("\nERROR: Expected (%.*s) == (%s)\n\n", size, match, expect_param[i]);
+	    return 1;
+	}
+	offset += read;
+	i++;
+    }
+
+    if (i < count) {
+	printf("\nERROR: Not enough matches. Expected %d but got %d.\n\n", count, i);
+	return 1;
+    }
     return 0;
 }
 
@@ -107,7 +194,7 @@ void print_literal(const char *msg) {
     printf("\033[1;31m%s\033[00m", msg);
 }
 
-int run_test(const char *example, const char **expect, int count) {
+int run_matcher_test(const char *example, int *expect_type, const char **expect_param, int count) {
     if (fancy)
 	printf("[....] ");
 
@@ -120,7 +207,59 @@ int run_test(const char *example, const char **expect, int count) {
     int i;
     for (i = 0; i < count; i++) {
 	if (i > 0)
-	    printf(",");
+	    printf(", ");
+	switch (expect_type[i]) {
+	    case M_WORD:
+		printf(":word");
+		break;
+	    case M_SUBSTRING:
+		printf(":substring");
+		break;
+	    case M_BEGINNING:
+		printf(":beginning");
+		break;
+	    case M_CASE:
+		printf(":case");
+		break;
+	    case M_NOT:
+		printf(":not");
+		break;
+	}
+        printf(" => ");
+	print_special("\"");
+	print_literal(expect_param[i]);
+	print_special("\"");
+    }
+
+    printf("]");
+    if (fancy)
+	printf("\r");
+    else
+	printf("\n");
+
+    if (run_enum_matchers_test(example, expect_type, expect_param, count)) {
+	printf("[\033[0;31mFAIL\033[00m]\n");
+	return 1;
+    } else {
+	printf("[\033[0;32mPASS\033[00m]\n");
+	return 0;
+    }
+}
+
+int run_field_test(const char *example, const char **expect, int count) {
+    if (fancy)
+	printf("[....] ");
+
+    printf("Testing (");
+    print_special("\"");
+    print_literal(example);
+    print_special("\"");
+    printf(") => [");
+
+    int i;
+    for (i = 0; i < count; i++) {
+	if (i > 0)
+	    printf(", ");
 	print_special("\"");
 	print_literal(expect[i]);
 	print_special("\"");
@@ -146,28 +285,43 @@ int main(int argc, char **argv) {
 
     fancy = argc == 2 ? 0 : 1;
 
-    result |= run_test("a", (const char*[]){"a"}, 1);
-    result |= run_test(" a ", (const char*[]){"a"}, 1);
+    printf("\n * MATCHER TESTS *\n");
+    result |= run_matcher_test(";w=\"foo\"", (int[]){M_WORD}, (const char*[]){"foo"}, 1);
+    result |= run_matcher_test(";s=\"foo\"", (int[]){M_SUBSTRING}, (const char*[]){"foo"}, 1);
+    result |= run_matcher_test(";b=\"foo\"", (int[]){M_BEGINNING}, (const char*[]){"foo"}, 1);
+    result |= run_matcher_test(";c", (int[]){M_CASE}, (const char*[]){""}, 1);
+    result |= run_matcher_test(";n", (int[]){M_NOT}, (const char*[]){""}, 1);
 
-    result |= run_test("a,b", (const char*[]){"a","b"}, 2);
-    result |= run_test(" a,b ", (const char*[]){"a","b"}, 2);
-    result |= run_test(",a,b,", (const char*[]){"a","b"}, 2);
-    result |= run_test(" ,a,b, ", (const char*[]){"a","b"}, 2);
+    result |= run_matcher_test(";c;w=\"foo\"", (int[]){M_CASE, M_WORD}, (const char*[]){"", "foo"}, 2);
+    result |= run_matcher_test(";n;s=\"foo\"", (int[]){M_NOT, M_SUBSTRING}, (const char*[]){"", "foo"}, 2);
 
-    result |= run_test("hello world, it's me", (const char*[]){"hello world","it's me"}, 2);
-    result |= run_test(" the quick brown fox ", (const char*[]){"the quick brown fox"}, 1);
-    result |= run_test(" the,quick ,brown, fox ", (const char*[]){"the","quick","brown","fox"}, 4);
-    result |= run_test("the,quick,brown,fox", (const char*[]){"the","quick","brown","fox"}, 4);
-    result |= run_test("the , quick, brown ,fox", (const char*[]){"the","quick","brown","fox"}, 4);
-    result |= run_test("the , , , quick, brown ,fox", (const char*[]){"the","quick","brown","fox"}, 4);
-    result |= run_test("a,\"b,c\"", (const char*[]){"a","\"b,c\""}, 2);
-    result |= run_test("a=\"b,c\"", (const char*[]){"a=\"b,c\""}, 1);
-    result |= run_test("\"a", (const char*[]){"\"a"}, 1);
-    result |= run_test("a\"", (const char*[]){"a\""}, 1);
-    result |= run_test("a \"escape\\\"qu,ote\"", (const char*[]){"a \"escape\\\"qu,ote\""}, 1);
-    result |= run_test("a \"escape\\", (const char*[]){"a \"escape\\"}, 1);
+    result |= run_matcher_test(";w=\"foo=\\\"bar\\\"\"", (int[]){M_WORD}, (const char*[]){"foo=\\\"bar\\\""}, 1);
 
-    result |= run_test("1, 2, a=\"b,c\"", (const char*[]){"1","2","a=\"b,c\""}, 3);
+    result |= run_matcher_test(";n;c;w=\"a\";s=\"b\";b=\"c\"", (int[]){M_NOT, M_CASE, M_WORD, M_SUBSTRING, M_BEGINNING}, (const char*[]){"", "", "a", "b", "c"}, 5);
+
+    printf("\n * FIELD TESTS * \n");
+    result |= run_field_test("a", (const char*[]){"a"}, 1);
+    result |= run_field_test(" a ", (const char*[]){"a"}, 1);
+
+    result |= run_field_test("a,b", (const char*[]){"a","b"}, 2);
+    result |= run_field_test(" a,b ", (const char*[]){"a","b"}, 2);
+    result |= run_field_test(",a,b,", (const char*[]){"a","b"}, 2);
+    result |= run_field_test(" ,a,b, ", (const char*[]){"a","b"}, 2);
+
+    result |= run_field_test("hello world, it's me", (const char*[]){"hello world","it's me"}, 2);
+    result |= run_field_test(" the quick brown fox ", (const char*[]){"the quick brown fox"}, 1);
+    result |= run_field_test(" the,quick ,brown, fox ", (const char*[]){"the","quick","brown","fox"}, 4);
+    result |= run_field_test("the,quick,brown,fox", (const char*[]){"the","quick","brown","fox"}, 4);
+    result |= run_field_test("the , quick, brown ,fox", (const char*[]){"the","quick","brown","fox"}, 4);
+    result |= run_field_test("the , , , quick, brown ,fox", (const char*[]){"the","quick","brown","fox"}, 4);
+    result |= run_field_test("a,\"b,c\"", (const char*[]){"a","\"b,c\""}, 2);
+    result |= run_field_test("a=\"b,c\"", (const char*[]){"a=\"b,c\""}, 1);
+    result |= run_field_test("\"a", (const char*[]){"\"a"}, 1);
+    result |= run_field_test("a\"", (const char*[]){"a\""}, 1);
+    result |= run_field_test("a \"escape\\\"qu,ote\"", (const char*[]){"a \"escape\\\"qu,ote\""}, 1);
+    result |= run_field_test("a \"escape\\", (const char*[]){"a \"escape\\"}, 1);
+
+    result |= run_field_test("1, 2, a=\"b,c\"", (const char*[]){"1","2","a=\"b,c\""}, 3);
 
     return result;
 }
